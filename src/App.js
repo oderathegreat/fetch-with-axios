23d221fc8489eaaab4f22c68f0030b8063ae8103
oderathegreat <oderathegreat@gmail.com>
2023-08-07 13:02:56 +0300

    inserting quote into paragraph tag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { useState } from 'react';
+
+
 
 function App() {
+
+  const [quote, setQuote] = useState('')
 
 const getQuote = () => {
   axios.get('https://api.quotable.io/random')
   .then(res => {
+    setQuote(res.data.content)
     console.log(res)
   }).catch(err => {
     console.log(err)
@@ -18,6 +24,7 @@ const getQuote = () => {
     <div className="App">
       
       <button onClick={getQuote} >  Todays Quote </button>
+      <p>{quote}</p>
     </div>
   );
 }
